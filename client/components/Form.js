ea4f44fdefc2b0b4613295a7f5dd80c384370061
Zachary Droge <zdroge@MacBook-Pro-3.local>
2021-12-06 07:44:31 -0600

    Added some question prompts for demo

--- client/components/Form.js
+++ client/components/Form.js
@@ -31,3 +31,22 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(null, mapDispatchToProps)(Form);
+
+
+/* 
+
+- Create a form that has a handle submit func
+  -this context and binding ????????
+- Use a handle change to allow for some front end (realtime) validation before form submission
+  - Why is this needed? Why not just let them submit the form and check the inputs?
+  - How do you handle the data in the form? Where to we store it?
+- How do we handle multiple inputs?
+  - What are some issues with how we have this current form filled out?
+- Lets Change the user inputs to upperCase.
+  - Did we let the user see this change?
+  - What is a controlled form???
+
+
+
+
+*/
--- public/bundle.js
+++ public/bundle.js
@@ -2220,6 +2220,23 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,react_redux__WEBPACK_IMPORTED_MODULE_1__.connect)(null, mapDispatchToProps)(Form));
+/* 
+
+- Create a form that has a handle submit func
+  -this context and binding ????????
+- Use a handle change to allow for some front end (realtime) validation before form submission
+  - Why is this needed? Why not just let them submit the form and check the inputs?
+  - How do you handle the data in the form? Where to we store it?
+- How do we handle multiple inputs?
+  - What are some issues with how we have this current form filled out?
+- Lets Change the user inputs to upperCase.
+  - Did we let the user see this change?
+  - What is a controlled form???
+
+
+
+
+*/
 
 /***/ }),
 
