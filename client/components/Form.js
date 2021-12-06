b6718719ec1e194ab87894c06bbda48675a8cac6
Zachary Droge <zdroge@MacBook-Pro-3.local>
2021-12-06 07:46:00 -0600

    Added some more question prompts for demo

--- client/components/Form.js
+++ client/components/Form.js
@@ -37,6 +37,8 @@ export default connect(null, mapDispatchToProps)(Form);
 
 - Create a form that has a handle submit func
   -this context and binding ????????
+  - event.preventDefault()
+  - What would we expect in our server from a form submission?
 - Use a handle change to allow for some front end (realtime) validation before form submission
   - Why is this needed? Why not just let them submit the form and check the inputs?
   - How do you handle the data in the form? Where to we store it?
@@ -45,8 +47,5 @@ export default connect(null, mapDispatchToProps)(Form);
 - Lets Change the user inputs to upperCase.
   - Did we let the user see this change?
   - What is a controlled form???
-
-
-
 
 */
--- public/bundle.js
+++ public/bundle.js
@@ -2224,6 +2224,8 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 
 - Create a form that has a handle submit func
   -this context and binding ????????
+  - event.preventDefault()
+  - What would we expect in our server from a form submission?
 - Use a handle change to allow for some front end (realtime) validation before form submission
   - Why is this needed? Why not just let them submit the form and check the inputs?
   - How do you handle the data in the form? Where to we store it?
@@ -2232,9 +2234,6 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 - Lets Change the user inputs to upperCase.
   - Did we let the user see this change?
   - What is a controlled form???
-
-
-
 
 */
 
